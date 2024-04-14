631c9e4358a18b3b50d57976a2a79c8ddaa93162
sichev <sichev@gmail.com>
2024-04-15 00:44:08 +0300

    Add setLength method to PasswordGenerator and extract check for numeric test

--- app/Models/PasswordGenerator.php
+++ app/Models/PasswordGenerator.php
@@ -4,13 +4,19 @@ namespace App\Models;
 
 class PasswordGenerator
 {
+    private int $length = 8;
 
+    public function setLength(int $length): self
+    {
+        $this->length = $length;
+        return $this;
+    }
 
-    public function getPassword(int $length = 8): string
+    public function getPassword(): string
     {
         $pass = "";
-        for ($i = 0; $i < $length; $i++) {
-            $pass .= mt_rand(0, 9);
+        for ($i = 0; $i < $this->length; $i++) {
+            $pass .= mt_rand(0, 1);
         }
         return $pass;
     }
--- tests/Unit/PasswordGeneratorTest.php
+++ tests/Unit/PasswordGeneratorTest.php
@@ -11,8 +11,17 @@ class PasswordGeneratorTest extends TestCase
     {
         $passLength = mt_rand(1, 16);
 
-        $password = (new PasswordGenerator)->getPassword($passLength);
+        $password = (new PasswordGenerator)->setLength($passLength)->getPassword();
         $this->assertNotEmpty($password);
         $this->assertEquals($passLength, strlen($password));
+    }
+
+    public function test_get_numeric_password()
+    {
+        $password = (new PasswordGenerator)->setLength(300)->getPassword();
+        $set = '1234567890';
+        for ($i = 0; $i < strlen($password)-1; $i++) {
+            $this->assertContains($password[$i], str_split($set));
+        }
     }
 }
