7dcf1630c9675be3c96d37dc61896b86517bbd57
sichev <sichev@gmail.com>
2024-04-15 02:51:04 +0300

    Refactor PasswordGenerator and corresponding tests
    The PasswordGenerator has been refactored to handle passwords using a character set approach instead of individual flags. New methods for processing and managing the character include requirement of unique characters' usage.

--- app/Models/PasswordGenerator.php
+++ app/Models/PasswordGenerator.php
@@ -4,12 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class PasswordGenerator extends Model
+class PasswordGenerator
 {
     private int $length = 8;
-    private bool $useNumerics = false;
-    private bool $useUpperCase = false;
-    private bool $useLowerCase = false;
+    private string $charactersSet = '';
 
     public function setLength(int $length): self
     {
@@ -18,41 +16,76 @@ class PasswordGenerator extends Model
     }
 
     public function useUpperCase(bool $upperCase = true): self {
-        $this->useUpperCase = $upperCase;
+        $set = config('app.passwordSets.upperCase');
+        $this->processSet($set, $upperCase);
         return $this;
     }
 
     public function useLowerCase(bool $lowerCase = true): self {
-        $this->useLowerCase = $lowerCase;
+        $set = config('app.passwordSets.lowerCase');
+        $this->processSet($set, $lowerCase);
         return $this;
     }
 
     public function useNumerics(bool $numerics = true): self {
-        $this->useNumerics = $numerics;
+        $set = config('app.passwordSets.numbers');
+        $this->processSet($set, $numerics);
         return $this;
+    }
+
+    public function getSet(): string
+    {
+        return $this->charactersSet;
     }
 
     public function getPassword(): string
     {
-        $set = $this->prepareSet();
-        $setLength = strlen($set) - 1;
-        if ($setLength <= 0) {
-            throw new \Exception("Password set is empty");
+        if (strlen($this->charactersSet) < $this->length) {
+            throw new \Exception("Password set is too short");
         }
+
+        $availableSet = $this->charactersSet;
 
         $pass = "";
         for ($i = 0; $i < $this->length; $i++) {
-            $pass .= $set[mt_rand(0, $setLength)];
+            $usedChar = $this->getRandomCharFromString($availableSet);
+            $pass .= $usedChar;
+            $availableSet = $this->removeCharFromSet($usedChar, $availableSet);
         }
         return $pass;
     }
 
-    private function prepareSet(): string
+    private function processSet(string $set, bool $isNeedToAdd)
     {
-        $set = "";
-        if ($this->useUpperCase) $set .= config('app.passwordSets.upperCase');
-        if ($this->useLowerCase) $set .= config('app.passwordSets.lowerCase');
-        if ($this->useNumerics) $set .= config('app.passwordSets.numbers');
-        return $set;
+        $this->removeSet($set);
+        if ($isNeedToAdd)
+            $this->addSet($set);
+        return $this;
+    }
+
+    private function removeCharFromSet(string $char, string $set): string
+    {
+        $set = str_split($set);
+        $set = array_diff($set, [$char]);
+        return implode('', $set);
+    }
+
+    private function removeSet(string $set): self
+    {
+        for ($i = 0; $i <= strlen($set)-1; $i++) {
+            $this->charactersSet = $this->removeCharFromSet($set[$i], $this->charactersSet);
+        }
+        return $this;
+    }
+
+    private function addSet(string $set): self
+    {
+        $this->charactersSet .= $set;
+        return $this;
+    }
+
+    private function getRandomCharFromString(string $string): string
+    {
+        return $string[mt_rand(0, strlen($string) - 1)];
     }
 }
--- tests/Unit/PasswordGeneratorTest.php
+++ tests/Unit/PasswordGeneratorTest.php
@@ -4,7 +4,6 @@ namespace Tests\Unit;
 
 use App\Models\PasswordGenerator;
 use Tests\TestCase;
-use Tests\CreatesApplication;
 
 class PasswordGeneratorTest extends TestCase
 {
@@ -15,16 +14,37 @@ class PasswordGeneratorTest extends TestCase
 
     public function test_get_password()
     {
-        $passLength = mt_rand(1, 16);
+        $passLength = mt_rand(3, 5);
 
         $password = (new PasswordGenerator)->setLength($passLength)->useNumerics()->getPassword();
         $this->assertNotEmpty($password);
         $this->assertEquals($passLength, strlen($password));
     }
 
+    public function test_sets_for_correct_set_and_unset()
+    {
+        $generator = new PasswordGenerator();
+        $this->assertEquals($generator->getSet(), '');
+
+        $generator->useNumerics();
+        $this->assertEquals($generator->getSet(), config('app.passwordSets.numbers'));
+        $generator->useNumerics(false);
+        $this->assertEquals($generator->getSet(), '');
+
+        $generator->useLowerCase();
+        $this->assertEquals($generator->getSet(), config('app.passwordSets.lowerCase'));
+        $generator->useLowerCase(false);
+        $this->assertEquals($generator->getSet(), '');
+
+        $generator->useUpperCase();
+        $this->assertEquals($generator->getSet(), config('app.passwordSets.upperCase'));
+        $generator->useUpperCase(false);
+        $this->assertEquals($generator->getSet(), '');
+    }
+
     public function test_get_numeric_password()
     {
-        $password = (new PasswordGenerator)->setLength(300)->useNumerics()->getPassword();
+        $password = (new PasswordGenerator)->setLength(10)->useNumerics()->getPassword();
         $set = config('app.passwordSets.numbers');
         for ($i = 0; $i < strlen($password)-1; $i++) {
             $this->assertContains($password[$i], str_split($set));
@@ -37,7 +57,7 @@ class PasswordGeneratorTest extends TestCase
 
     public function test_get_lowercase_password()
     {
-        $password = (new PasswordGenerator)->setLength(300)->useLowerCase()->getPassword();
+        $password = (new PasswordGenerator)->setLength(26)->useLowerCase()->getPassword();
         $set = config('app.passwordSets.lowerCase');
         for ($i = 0; $i < strlen($password)-1; $i++) {
             $this->assertContains($password[$i], str_split($set));
@@ -50,7 +70,7 @@ class PasswordGeneratorTest extends TestCase
 
     public function test_get_uppercase_password()
     {
-        $password = (new PasswordGenerator)->setLength(300)->useUpperCase()->getPassword();
+        $password = (new PasswordGenerator)->setLength(26)->useUpperCase()->getPassword();
         $set = config('app.passwordSets.upperCase');
         for ($i = 0; $i < strlen($password)-1; $i++) {
             $this->assertContains($password[$i], str_split($set));
@@ -58,6 +78,32 @@ class PasswordGeneratorTest extends TestCase
 
         for ($i = 0; $i < strlen($set)-1; $i++) {
             $this->assertContains($set[$i], str_split($password));
+        }
+    }
+
+    public function test_password_excess_length()
+    {
+        $this->expectExceptionMessage("Password set is too short");
+        $password = (new PasswordGenerator)
+            ->setLength(11)
+            ->useNumerics()
+            ->getPassword();
+    }
+
+
+    public function test_password_for_unique_characters()
+    {
+        $password = (new PasswordGenerator)
+            ->setLength(10 + 26 + 26)
+            ->useNumerics()
+            ->useLowerCase()
+            ->useUpperCase()
+            ->getPassword();
+
+        $usedCharacters = [];
+        for ($i = 0; $i < strlen($password); $i++) {
+            $this->assertNotContains($password[$i], $usedCharacters);
+            $usedCharacters[] = $password[$i];
         }
     }
 }
