9efc23455a71bd163e612500009e21a6a0b805fe
sichev <sichev@gmail.com>
2024-04-15 01:19:59 +0300

    Enhance PasswordGenerator with custom sets features

--- app/Models/PasswordGenerator.php
+++ app/Models/PasswordGenerator.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
-class PasswordGenerator
+use Illuminate\Database\Eloquent\Model;
+
+class PasswordGenerator extends Model
 {
     private int $length = 8;
+    private bool $useNumerics = false;
+    private bool $useUpperCase = false;
+    private bool $useLowerCase = false;
 
     public function setLength(int $length): self
     {
@@ -12,12 +17,42 @@ class PasswordGenerator
         return $this;
     }
 
+    public function useUpperCase(bool $upperCase = true): self {
+        $this->useUpperCase = $upperCase;
+        return $this;
+    }
+
+    public function useLowerCase(bool $lowerCase = true): self {
+        $this->useLowerCase = $lowerCase;
+        return $this;
+    }
+
+    public function useNumerics(bool $numerics = true): self {
+        $this->useNumerics = $numerics;
+        return $this;
+    }
+
     public function getPassword(): string
     {
+        $set = $this->prepareSet();
+        $setLength = strlen($set) - 1;
+        if ($setLength <= 0) {
+            throw new \Exception("Password set is empty");
+        }
+
         $pass = "";
         for ($i = 0; $i < $this->length; $i++) {
-            $pass .= mt_rand(0, 1);
+            $pass .= $set[mt_rand(0, $setLength)];
         }
         return $pass;
+    }
+
+    private function prepareSet(): string
+    {
+        $set = "";
+        if ($this->useUpperCase) $set .= config('app.passwordSets.upperCase');
+        if ($this->useLowerCase) $set .= config('app.passwordSets.lowerCase');
+        if ($this->useNumerics) $set .= config('app.passwordSets.numbers');
+        return $set;
     }
 }
--- tests/Unit/PasswordGeneratorTest.php
+++ tests/Unit/PasswordGeneratorTest.php
@@ -3,25 +3,61 @@
 namespace Tests\Unit;
 
 use App\Models\PasswordGenerator;
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use Tests\CreatesApplication;
 
 class PasswordGeneratorTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        $this->createApplication();
+    }
+
     public function test_get_password()
     {
         $passLength = mt_rand(1, 16);
 
-        $password = (new PasswordGenerator)->setLength($passLength)->getPassword();
+        $password = (new PasswordGenerator)->setLength($passLength)->useNumerics()->getPassword();
         $this->assertNotEmpty($password);
         $this->assertEquals($passLength, strlen($password));
     }
 
     public function test_get_numeric_password()
     {
-        $password = (new PasswordGenerator)->setLength(300)->getPassword();
-        $set = '1234567890';
+        $password = (new PasswordGenerator)->setLength(300)->useNumerics()->getPassword();
+        $set = config('app.passwordSets.numbers');
         for ($i = 0; $i < strlen($password)-1; $i++) {
             $this->assertContains($password[$i], str_split($set));
+        }
+
+        for ($i = 0; $i < strlen($set)-1; $i++) {
+            $this->assertContains($set[$i], str_split($password));
+        }
+    }
+
+    public function test_get_lowercase_password()
+    {
+        $password = (new PasswordGenerator)->setLength(300)->useLowerCase()->getPassword();
+        $set = config('app.passwordSets.lowerCase');
+        for ($i = 0; $i < strlen($password)-1; $i++) {
+            $this->assertContains($password[$i], str_split($set));
+        }
+
+        for ($i = 0; $i < strlen($set)-1; $i++) {
+            $this->assertContains($set[$i], str_split($password));
+        }
+    }
+
+    public function test_get_uppercase_password()
+    {
+        $password = (new PasswordGenerator)->setLength(300)->useUpperCase()->getPassword();
+        $set = config('app.passwordSets.upperCase');
+        for ($i = 0; $i < strlen($password)-1; $i++) {
+            $this->assertContains($password[$i], str_split($set));
+        }
+
+        for ($i = 0; $i < strlen($set)-1; $i++) {
+            $this->assertContains($set[$i], str_split($password));
         }
     }
 }
